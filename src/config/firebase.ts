756f6c1e8612c1dd4cd7083478ef9d00ea330df1
Andrew Brown <arbrown83@gmail.com>
2026-03-09 19:16:01 -0400

    FIxing firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,12 +7,13 @@ import { getFirestore } from "firebase/firestore";
 // You can find them in the Firebase Console under:
 // Project Settings > General > Your apps > Web app
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+    apiKey: "REMOVED",
+    authDomain: "REMOVED",
+    projectId: "REMOVED",
+    storageBucket: "REMOVED",
+    messagingSenderId: "REMOVED",
+    appId: "REMOVED",
+    measurementId: "REMOVED"
 };
 
 const app = initializeApp(firebaseConfig);
